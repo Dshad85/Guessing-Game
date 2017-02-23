8dd20cadbcc1874635f3c67d0537f89152e348bc
Dshad85 <d.shad85@gmail.com>
2017-02-22 16:12:45 -0800

    A number guessing game
    Guess a number between 1 and 20. You get five tries.

---
+++ Guessing_game.py
@@ -0,0 +1,19 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+import random
+number = random.choice(numbers)
+for i in range(1, 6):
+
+    guess = raw_input("Guess a number between 1 and 20: ")
+    guess = int(guess)
+
+    if guess == number:
+        print "Great job! you win"
+        break
+    elif guess > 20:
+        print "Oops, that number is not in 1 through 20!"
+    else:
+        print "That is not correct."
+
+print "The correct answer was %d." % number
+print "All done."
